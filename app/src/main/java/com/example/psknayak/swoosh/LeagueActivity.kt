f4e1bbcec3c12757d8caca3df6a3affb87123748
Karthik S Nayak <psknayak@gmail.com>
2018-07-15 00:57:24 +0530

    Buttoned up the Activities

--- app/src/main/java/com/example/psknayak/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/psknayak/swoosh/LeagueActivity.kt
@@ -1,20 +1,50 @@
 package com.example.psknayak.swoosh
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view:View)
-    {
-        val skillActivity = Intent(this, SkillActivity::class.java )
-        startActivity(skillActivity)
+    fun onMensClicked(view: View) {
+        womensToggle.isChecked = false
+        kidsToggle.isChecked = false
+
+        selectedLeague = "Men's"
+
+    }
+
+    fun onWomensClicked(view: View) {
+        mensToggle.isChecked = false
+        kidsToggle.isChecked = false
+
+        selectedLeague = "Women's"
+    }
+
+    fun onKidsClicked(view: View) {
+        mensToggle.isChecked = false
+        womensToggle.isChecked = false
+
+        selectedLeague = "Kid's"
+    }
+
+    fun leagueNextClicked(view: View) {
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE,selectedLeague)
+            startActivity(skillActivity)
+        }
+        else
+        {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/psknayak/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/psknayak/swoosh/SkillActivity.kt
@@ -2,11 +2,39 @@ package com.example.psknayak.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_skill.*
 
-class SkillActivity : AppCompatActivity() {
-
+class SkillActivity : BaseActivity()
+{
+    var league = ""
+    var selectedSkill = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
+    }
+
+    fun onBeginnerClicked(view: View)
+    {
+        ballerToggle.isChecked = false
+
+        selectedSkill = "Beginner"
+    }
+
+    fun onBallerClicked(view: View)
+    {
+        beginnerToggle.isChecked = false
+
+        selectedSkill = "Baller"
+    }
+
+    fun finishClicked(view: View)
+    {
+        if(selectedSkill==="")
+            Toast.makeText(this, "Please mention your skill level.", Toast.LENGTH_SHORT).show()
     }
 }
+
