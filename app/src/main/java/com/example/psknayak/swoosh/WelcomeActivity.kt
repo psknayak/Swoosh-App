2a125618af9c2d614df191325a8faa764fc83690
Karthik S Nayak <psknayak@gmail.com>
2018-07-14 21:05:57 +0530

    Used Intents and Contexts

--- app/src/main/java/com/example/psknayak/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/psknayak/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.psknayak.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java) //explicit intent this is a context for this activity
+            startActivity(leagueIntent)
+
+        }
     }
 }
